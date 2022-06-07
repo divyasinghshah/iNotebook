c327e68fc24bfee17f7788704eae80710c2bfd53
divyasinghshah <divyasinghshah@gmail.com>
2022-06-07 21:35:45 +0530

    Show all notes and add new notes of logged in user

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,7 +2,9 @@ const express=require('express');
 const router=express.Router();
 const fetchUser=require('../middleware/fetchUser');
 const Notes=require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
+//Router 1:: fetch all notes using get :login required.
 router.get('/fetchallnotes',fetchUser ,async(req,res)=>{
     try{
         let notes=await Notes.find({user:req.user.id});
@@ -15,6 +17,38 @@ router.get('/fetchallnotes',fetchUser ,async(req,res)=>{
 
     }
     
+});
+
+// Router 2:: add new note using post:login required
+router.post('/addnote',[
+    body('title', 'enter valid title').isLength({min:5}),
+    body('description','description length should be atleast 5').isLength({ min: 5 })
+
+],fetchUser, async(req,res)=>{
+    try{
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+
+        let notes=await Notes.create({
+            title:req.body.title,
+            description:req.body.description,
+            tag:req.body.tag,
+            user:req.user.id
+        })
+
+        return res.json({notes});
+
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Some error occured");
+
+    }
+
 })
+
 
 module.exports=router;
