22e494bf746e189f417acf1d47ebcdcfa31263cd
divyasinghshah <divyasinghshah@gmail.com>
2022-06-09 20:46:53 +0530

    Added Alert component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import { Home } from './components/Home';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
 import NoteState from './context/notes/noteState';
+import Alert from './components/Alert';
 function App() {
   return (
     <>
     
     <BrowserRouter>
     <Navbar/>
+    <Alert message="Hello world"/>
     <NoteState>
     <div className="container">
     <Routes>
