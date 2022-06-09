6481775772bdffda4aad50f2f50372ea354f7061
divyasinghshah <divyasinghshah@gmail.com>
2022-06-09 20:45:11 +0530

    Added icons

--- src/components/NoteItems.js
+++ src/components/NoteItems.js
@@ -5,9 +5,17 @@ const NoteItems = (props) => {
     return (
         <div className='col-md-3'>
 
-
             <div className="card" >
                 <div class="card-body">
+                    <div className="d-flex justify-content-between">
+                    <h5 class="card-title">{note.title}</h5>
+                    <div>
+                        <i className="fa-solid fa-trash-can mx-2"></i>
+                        <i className="fa-solid fa-pen-to-square mx-2"></i>
+                    </div>
+                    
+
+                    </div>
                     <h5 class="card-title">{note.title}</h5>
                     <p class="card-text">{note.description}</p>
                 </div>
